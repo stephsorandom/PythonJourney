ee0c4f3476d89d4a0541010a33c875610f11d2ca
Stephanie Mitchell <stephsorandom@gmail.com>
2021-07-27 21:26:45 -0400

    Numbers

---
+++ Numbers.py
@@ -0,0 +1,11 @@
+7+5
+1/2
+10-8
+
+
+Modulo Operator - Returns the remainder AFTER division. 
+Ex: 7 / 4 = 1.75
+    7 % 4 = 3
+
+    - Useful Operator when you need to find if a number is even or odd. 
+    - # % 2 => if anything other than 0, then the number is odd!
