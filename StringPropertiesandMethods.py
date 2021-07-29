e43b5c735180deb9e7166a9de6604ff4d8dda2d4
Stephanie Mitchell <stephsorandom@gmail.com>
2021-07-28 21:26:26 -0400

    String lesson covered

--- StringPropertiesandMethods.py
+++ StringPropertiesandMethods.py
@@ -23,3 +23,31 @@
         letter.upper() => ZZZZZ
         letter.lower() => zzzzz
         letter.split() => ['z','z', 'z', 'z', 'z', 'z']
+
+
+
+- String Interpolation 
+
+.format() method: Allows you to insert into a print statment by using object format
+    EX: print('This is a string {}'.format('INSERTED'))  
+                => 'This is a string INSERTED'
+
+You can use index positioning with .format to insert in any desired order 
+    EX1: print('The {2} {1} {3}'.format('fox', 'brown', 'quick'))
+                => 'The quick brown fox'
+    EX2: print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+                => 'The quick brown fox'
+
+
+
+- Float Formatting
+
+Allows you to adjust the width and precision of floating point number
+                print('The result {}'.format(value:width.precision f))
+
+- F strings (Formatted String Literals)
+
+Allows you to skip the .format() and write the variable directly inside the string.
+    EX1: name = 'Jose'
+        print(f'Hello, his name is {name}') 
+                        => 'Hello, his name is Jose'   =========>> New to Python 3.0!!
