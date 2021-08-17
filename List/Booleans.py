a52aa463e504845010bc681f070759451c960042
Stephanie Mitchell <stephsorandom@gmail.com>
2021-08-17 13:30:50 -0400

    Booleans quick lesson

---
+++ List/Booleans.py
@@ -0,0 +1,14 @@
+Booleans are statement that show if that condition is met, will it either be True or False
+
+    ** Sure you capitalize the first letter: 
+                    True        or       False
+
+
+    Ex: 
+        1 > 2 
+        = True
+
+
+~ You can set a variable to be none..incase you need to use it for later on but want to declare it now. 
+            b = None 
+                    ==> Will not give kyou an error when declared until you are ready to assign it a value
