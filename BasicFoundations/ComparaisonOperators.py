9b856333e03070ae48a3ac2464d9b915434000cc
Stephanie Mitchell <stephsorandom@gmail.com>
2021-09-01 21:50:13 -0400

    comparison operators

---
+++ BasicFoundations/ComparaisonOperators.py
@@ -0,0 +1,21 @@
+Comparision Operators can be : 
+                    and or not 
+    
+    AND EX:
+    1 < 2 < 3 ===>>>> True. Because 1 is less than 2 and 2 is less than 3
+    1 < 2 and 2 < 3 ===>> True. Because compares both sides to see if true or false. 
+
+    'h' == 'h' and 2 == 2 ====>>> True! 
+
+
+    OR EX: 
+    1 == 1 OR 2 == 3 ===>>> TRUE! With 'or' just needs 1 arguement to be true. 
+
+
+    NOT EX:
+    You return the opposite boolean of what is being asked. 
+
+        not 1 == 1  =>>> False
+        400 > 5000  =>>> False, because 400 is less
+        not 400 > 5000 ===> True. Because 400 is less than 5000, so it is NOT true 
+        
