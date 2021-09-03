7dae538da0957962fe55df7e224800e8f9d03333
Stephanie Mitchell <stephsorandom@gmail.com>
2021-09-02 22:44:12 -0400

    section 4 of Python

---
+++ BasicFoundations/Operators/IfElseStatments.py
@@ -0,0 +1,4 @@
+If, Elif, Else Statements
+
+    ~ Control Flow Syntax in Python use of colons, indentation and whitespace 
+        • This is VERY important and sets Python apart from other programming languages. 
