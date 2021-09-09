3eca7563a32cfd3c5162c9bdef0a1b3c71adc1b4
Stephanie Mitchell <stephsorandom@gmail.com>
2021-09-09 17:49:25 -0400

    Beginning the journey into functions in python!

---
+++ Functions and Methods/Methods.py
@@ -0,0 +1,18 @@
+Methods are basically built in objects into Functions
+
+mylist = [1,2,3]
+
+mylist.append(4) ##adds 4 into list
+    ==> [1,2,3,4]
+    
+
+mylist.pop() ##removes last item on list
+    ==> [1,2,3]
+
+help(mylist.insert)  ##built into python it will return the documentation of what you are looking for
+    ==> Help on built-in function.insert:
+            insert(..) method of builtins.list instance
+                L.insert(index, object) -- insert object before index
+
+    Or you can view the Python documentations online!
+
